add7e8fac225745aa60866aacd3b036b1d5d1bad
Ben King <ben.kingsy@gmail.com>
2018-01-05 11:28:43 +0000

    Add Styled Components

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
-import logo from './react.svg';
-import './Home.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 500px;
+  margin: 0 auto;
+  font-family: system-ui;
+  color: red;
+`;
 
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to Razzle</h2>
-        </div>
-        <p className="Home-intro">
-          To get started, edit <code>src/App.js</code> or{' '}
-          <code>src/Home.js</code> and save to reload.
-        </p>
-        <ul className="Home-resources">
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://palmer.chat">Community Slack</a>
-          </li>
-        </ul>
-      </div>
+      <Wrapper>
+        <h1>Home</h1>
+        <p>Paragraph.</p>
+      </Wrapper>
     );
   }
 }
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StaticRouter } from 'react-router-dom';
 import express from 'express';
 import { renderToString } from 'react-dom/server';
+import { ServerStyleSheet } from 'styled-components';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
@@ -12,11 +13,16 @@ server
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
     const context = {};
+    const sheet = new ServerStyleSheet();
     const markup = renderToString(
-      <StaticRouter context={context} location={req.url}>
-        <App />
-      </StaticRouter>
+      sheet.collectStyles(
+        <StaticRouter context={context} location={req.url}>
+          <App />
+        </StaticRouter>
+      )
     );
+
+    const styleTags = sheet.getStyleTags();
 
     if (context.url) {
       res.redirect(context.url);
@@ -29,12 +35,17 @@ server
         <meta charSet='utf-8' />
         <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${assets.client.css
-          ? `<link rel="stylesheet" href="${assets.client.css}">`
-          : ''}
-        ${process.env.NODE_ENV === 'production'
-          ? `<script src="${assets.client.js}" defer></script>`
-          : `<script src="${assets.client.js}" defer crossorigin></script>`}
+        ${
+          assets.client.css
+            ? `<link rel="stylesheet" href="${assets.client.css}">`
+            : ''
+        }
+        ${
+          process.env.NODE_ENV === 'production'
+            ? `<script src="${assets.client.js}" defer></script>`
+            : `<script src="${assets.client.js}" defer crossorigin></script>`
+        }
+        ${styleTags ? styleTags : null}
     </head>
     <body>
         <div id="root">${markup}</div>
