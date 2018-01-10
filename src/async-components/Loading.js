823a7c6162f34a2e6005526f73dc0e82c92e638b
Ben King <ben.kingsy@gmail.com>
2018-01-10 17:01:17 +0000

    Update loading component

--- src/async-components/Loading.js
+++ src/async-components/Loading.js
@@ -3,9 +3,7 @@ import React from 'react';
 export default function Loading(props) {
   if (props.error) {
     return <div>Error!</div>;
-  } else if (props.pastDelay) {
-    return <div>Loading...</div>;
   } else {
-    return null;
+    return <div>Loading route module...</div>;
   }
 }
