a407f1870eb1b86f60f5b589d7ca2c77c3f50d43
Ben King <ben.kingsy@gmail.com>
2018-01-15 18:22:08 +0000

    Add magic comment names to webpack imports

--- src/components/WebShell/About/index.jsx
+++ src/components/WebShell/About/index.jsx
@@ -2,6 +2,7 @@ import Loadable from 'react-loadable';
 import Loading from 'festicket/components/Loading';
 
 export default Loadable({
-  loader: () => import('festicket/components/WebShell/About/component'),
+  loader: () =>
+    import(/* webpackChunkName: 'About' */ 'festicket/components/WebShell/About/component'),
   loading: Loading
 });
--- src/components/WebShell/Festival/index.jsx
+++ src/components/WebShell/Festival/index.jsx
@@ -2,6 +2,7 @@ import Loadable from 'react-loadable';
 import Loading from 'festicket/components/Loading';
 
 export default Loadable({
-  loader: () => import('festicket/components/WebShell/Festival/component'),
+  loader: () =>
+    import(/* webpackChunkName: 'Festival' */ 'festicket/components/WebShell/Festival/component'),
   loading: Loading
 });
